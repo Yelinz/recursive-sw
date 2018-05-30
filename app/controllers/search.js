b5e926ab4056482fba18b087a7da2fe80cfd0500
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2018-05-30 11:20:11 +0200

    Changed to debounce searching

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,5 +1,5 @@
 import Controller from "@ember/controller"
-import { computed } from "@ember/object"
+import { debounce } from "@ember/runloop"
 
 export default Controller.extend({
   queryParams: ["search"],
@@ -8,5 +8,13 @@ export default Controller.extend({
     this._super(...arguments)
     this.set("checkboxes", {})
     this.set("checkboxes.people", true)
+  },
+  setSearch(value) {
+    this.set("search", value)
+  },
+  actions: {
+    handleSearch(value) {
+      debounce(this, this.setSearch, value, 500)
+    }
   }
 })
